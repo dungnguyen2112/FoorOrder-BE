457e31b1370e6b06dc9b6e4644bbd414b1fc87ce
dungnguyen2112 <nguyentridung20044@gmail.com>
2025-04-12 21:39:14 +0700

    update

--- src/main/java/com/example/cosmeticsshop/config/OpenAPIConfig.java
+++ src/main/java/com/example/cosmeticsshop/config/OpenAPIConfig.java
@@ -55,7 +55,8 @@ public class OpenAPIConfig {
         return new OpenAPI()
                 .info(createApiInfo())
                 .servers(List.of(
-                        createServer("http://localhost:8080", "Server URL in Development environment")))
+                        createServer("https://foororder-be-production.up.railway.app",
+                                "Server URL in Development environment")))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
     }
